611a2a35e88fca171c0f6f1d9f84143a87a6492b
Brian Logan <id@brianjlogan.com>
2023-04-19 23:16:39 -0400

    https://youtu.be/XZtlD_m59sM?t=675

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,35 @@
 use std::net::SocketAddr;
 
 use axum::Router;
-use axum::extract::Query;
-use axum::routing::get;
+use axum::extract::{Query, Path};
+use axum::routing::{get, get_service};
 use axum::response::{Html, IntoResponse};
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
     
-    let routes_hello = Router::new().route("/hello",get(handler_hello));
+    let routes_all = Router::new().merge(routes_hello());
 
     // region: ---Start Server
     let addr = SocketAddr::from(([127,0,0,1], 8080));
     println!("->> LISTENING ON {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+//region: ---- Routes Hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello",get(handler_hello))
+        .route("/hello2/:name",get(handler_hello2));
 }
 
 #[derive(Debug, Deserialize)]
@@ -28,6 +39,7 @@ struct HelloParams {
     name: Option<String>,
 }
 
+// e.g., `/hello?name=Jen`
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
 
     println!("->> {:12} - handler_hello - {params:?}", "HANDLER");
@@ -35,4 +47,11 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     let name = params.name.as_deref().unwrap_or("World!");
 
     Html(format!("Hello <strong>{name}</strong>"))
+}
+
+// e.g. `/hello2/Mike`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello2 <strong>{name}</strong>"))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
     
-    hc.do_get("/hello?name=Jen").await?.print().await?;
+    hc.do_get("/hello2/Mike").await?.print().await?;
 
     Ok(())
 }
